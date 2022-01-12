57fbe80aa5cf18605907d6121ceed40d1d87dd28
issuarez <issuarez@aidacanarias.com>
2022-01-12 17:27:31 +0100

    ✅ TripService should get no trips for no logged user with no friends and no trips

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -58,11 +58,27 @@ namespace TripServiceKata.Tests
 		[Fact]
 		public void Get_no_trips_for_logged_user_with_friends_and_trips()
 		{
-			static List<Trip> Trips(User _) => new List<Trip> { new Trip() };
+			static List<Trip> Trips(User _) => new List<Trip> {new Trip()};
 			User         user        = new User();
-			IUserSession userSession = new UserSessionStub { LoggedUser = user };
+			IUserSession userSession = new UserSessionStub {LoggedUser = user};
 			TripService  suo         = new TripService(userSession, Trips);
 			user.AddFriend(new User());
+
+			List<Trip> getSuoTrips = suo.GetTripsByUser(user);
+
+			getSuoTrips
+					.Should()
+					.Equal(new List<Trip>());
+		}
+
+		[Fact]
+		public void Get_no_trips_for_no_logged_user_with_no_friends_and_no_trips()
+		{
+			static List<Trip> NoTrips(User _) => new List<Trip>();
+			User         user        = new User();
+			User         loggedUser  = new User();
+			IUserSession userSession = new UserSessionStub {LoggedUser = loggedUser};
+			TripService  suo         = new TripService(userSession, NoTrips);
 
 			List<Trip> getSuoTrips = suo.GetTripsByUser(user);
 
