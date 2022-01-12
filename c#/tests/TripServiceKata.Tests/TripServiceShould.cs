1ed2aefa38f8324d40e76a462ec6a95ff380ff5d
issuarez <issuarez@aidacanarias.com>
2022-01-12 16:51:56 +0100

    ✅ TripService should not allow not logged user

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,28 @@
-﻿namespace TripServiceKata.Tests
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using TripServiceKata.Entity;
+using TripServiceKata.Exception;
+using TripServiceKata.Service;
+using Xunit;
+
+namespace TripServiceKata.Tests
 {
-    public class TripServiceShould
-    {
-    }
+	public class TripServiceShould
+	{
+		[Fact]
+		public void Not_allow_not_logged_user()
+		{
+			var user = new User();
+			IUserSession userSession = new UserSessionStub();
+			List<Trip> NoTrips(User _) => new List<Trip>();
+			var suo = new TripService(userSession, NoTrips);
+
+			Action getSuoTrips = () => suo.GetTripsByUser(user);
+
+			getSuoTrips
+					.Should()
+					.Throw<UserNotLoggedInException>();
+		}
+	}
 }
