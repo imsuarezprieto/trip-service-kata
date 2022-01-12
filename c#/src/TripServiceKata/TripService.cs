3386f65092d5738b2d73e34f9231c92a4fd448d8
issuarez <issuarez@aidacanarias.com>
2022-01-12 18:03:08 +0100

    ♻ Inline predicate condition

--- c#/src/TripServiceKata/TripService.cs
+++ c#/src/TripServiceKata/TripService.cs
@@ -10,30 +10,28 @@ namespace TripServiceKata
 	public class TripService
 	{
 		private readonly Func<User, List<Trip>> findTripsByUser;
-		private readonly IUserSession userSession;
+		private readonly IUserSession           userSession;
 
 		public TripService()
 		{
-			userSession = UserSession.GetInstance();
+			userSession     = UserSession.GetInstance();
 			findTripsByUser = TripDAO.FindTripsByUser;
 		}
 
 		public TripService(IUserSession userSession, Func<User, List<Trip>> findTripsByUser)
 		{
-			this.userSession = userSession;
+			this.userSession     = userSession;
 			this.findTripsByUser = findTripsByUser;
 		}
 
 		public List<Trip> GetTripsByUser(User user)
 		{
-			var loggedUser = userSession.GetLoggedUser();
+			User loggedUser = userSession.GetLoggedUser();
 			if (loggedUser == null) throw new UserNotLoggedInException();
-			var tripList   = new List<Trip>();
-			var isFriend = user.GetFriends()
-					.Any(friend => friend == loggedUser);
-			if (isFriend) tripList = findTripsByUser(user);
+			List<Trip> tripList = new List<Trip>();
+			if (user.GetFriends().Any(friend => friend == loggedUser))
+				tripList = findTripsByUser(user);
 			return tripList;
-
 		}
 	}
 }
