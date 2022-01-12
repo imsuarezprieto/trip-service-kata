2c5b58832665145024baaa586365615f8fc32223
issuarez <issuarez@aidacanarias.com>
2022-01-12 16:25:01 +0100

    🤡 UserSession stub

---
+++ c#/tests/TripServiceKata.Tests/UserSessionStub.cs
@@ -0,0 +1,12 @@
+﻿using TripServiceKata.Entity;
+using TripServiceKata.Service;
+
+namespace TripServiceKata.Tests
+{
+	class UserSessionStub : IUserSession
+	{
+		internal User User { private get; set;  }
+
+		public User GetLoggedUser() => User;
+	}
+}
